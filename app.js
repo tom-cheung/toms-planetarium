8d381c5416375d4aae76023c1cd67022c7b7ca33
tom-cheung <wai.tomcheung@gmail.com>
2021-01-06 21:53:06 -0800

    prepare for heroku deployment

--- app.js
+++ app.js
@@ -15,8 +15,13 @@ app.use('/build', express.static(path.join(__dirname, 'node_modules/three/build'
 
 app.use('/jsm', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
 
+let port = process.env.PORT; 
 
-app.listen(5000, () => {
+if (port == null || port == "") {
+    port = 5000; 
+}
+
+app.listen(port, () => {
     console.log('listening on 5000')
 })
 
